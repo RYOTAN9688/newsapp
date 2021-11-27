6b3a872ffb70fb22ff197d63bdce96b8421aaa09
RYOTAN9688 <ryota8813@gmail.com>
2021-11-27 23:45:59 +0900

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import styles from '../styles/Home.module.scss';
 import { Article } from './components/article';
 import { MainLayout } from './components/layouts';
 
+const NewsKey = process.env.NEWSKEY;
+
 export default function Home(props) {
   return (
     <>
@@ -27,7 +29,7 @@ export default function Home(props) {
 export const getStaticProps: GetStaticProps = async () => {
   const pageSize = 10;
   const topRes = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=bb57f3c40a3c4c1aa50c6da4dc8b7fe5`,
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${NewsKey}`,
   );
   const topJson = await topRes.json();
   const topArticles = topJson?.articles;
