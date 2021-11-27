0d663c08a7a967fdfad5e86208bd25f05e45e6c6
RYOTAN9688 <ryota8813@gmail.com>
2021-11-28 00:00:51 +0900

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home(props) {
 export const getStaticProps: GetStaticProps = async () => {
   const pageSize = 10;
   const topRes = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=bb57f3c40a3c4c1aa50c6da4dc8b7fe5`,
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${process.env.NEWSKEY}`,
   );
   const topJson = await topRes.json();
   const topArticles = topJson?.articles;
