0eb573783f9eee49348470880873bfdd15314e5c
RYOTAN9688 <ryota8813@gmail.com>
2021-11-28 08:38:05 +0900

    created Topicpage

--- src/pages/topics/[id].tsx
+++ src/pages/topics/[id].tsx
@@ -1,16 +1,18 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/dist/client/router';
 
 import { Article } from '../components/article';
 import { Nav } from '../components/nav';
 import { MainLayout } from '../components/layouts';
+
 import styles from '../../styles/Home.module.scss';
-import { GetStaticPaths, GetStaticProps } from 'next';
 
 const NewsKey = process.env.NEWSKEY;
 
 const Topic = (props) => {
   const router = useRouter();
+
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
@@ -25,7 +27,7 @@ const Topic = (props) => {
       <div className={styles.contents}>
         <div className={styles.blank}>
           <div className={styles.main}>
-            <Article title={props.title} articles={props.topicArticle} />
+            <Article title={props.title} articles={props.topicArticles} />
           </div>
         </div>
       </div>
@@ -41,15 +43,16 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params.id;
+
   const topicRes = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=jp&category=${params.id}&country=jp&apiKey=${NewsKey}`,
+    `https://newsapi.org/v2/top-headlines?country=jp&category=${params.id}&apiKey=${NewsKey}`,
   );
 
   const topicJson = await topicRes.json();
   const topicArticles = await topicJson.articles;
 
-  const title = params.id;
-  console.log(title);
+  const title = id;
 
   return {
     props: { topicArticles, title },
